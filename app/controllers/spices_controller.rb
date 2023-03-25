8f5393fcfcfea7c24759f568a8f5d59e0b0b2713
Miluche <meshkipla@gmail.com>
2023-03-25 15:32:09 +0300

    added routes and tested application

--- app/controllers/spices_controller.rb
+++ app/controllers/spices_controller.rb
@@ -1,2 +1,34 @@
 class SpicesController < ApplicationController
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  def index
+    spices = Spice.all
+    render json: spices
+  end
+
+  def create
+    spice = Spice.create(spice_params)
+    render json: spice, status: :created
+  end
+
+  def update
+    spice = Spice.find_by(id: params[:id])
+    spice.update(spice_params)
+    render json: spice
+  end
+
+  def destroy
+    spice = Spice.find_by(id: params[:id])
+    spice.destroy
+    head :no_content
+  end
+
+  private
+
+  def render_not_found_response
+    render json: {error: "Spice not found"}, status: :not_found
+  end
+  def spice_params
+    params.permit(:title, :image, :description , :notes, :rating)
+  end
 end
